40414377dfb910edb5b00a704097fce5a0fd0da1
Amina <symohcpt025@student.wethinkcode.co.za>
2026-09-15 12:08:10 +0200

    feat: formatted the logs correctly with string method

--- event.py
+++ event.py
@@ -3,18 +3,28 @@ class Event:
     def __init__(self, event_type, user, ip_address, event_details):
 
         self.timestamp = datetime.now()
-        self.event_type = event_type
-        self.user = user
+        self.event_type = event_type.upper()
+        self.user = user.lower()
         self.ip_address = ip_address
         self.event_details = event_details
+    
+    def __str__(self):
+        return f"{self.timestamp} {self.event_type} user={self.user} ip={self.ip_address} details={self.event_details}"
 
-date_time = datetime.now()
 # print(date_time) 
 event = Event(
-    event_type= "Attack",
+    event_type= "Successful login",
     event_details= "information",
     user= "Amina Mohiudeen",
     ip_address= "10.200.131.93",   
 )
 
-print(event.timestamp)
+event2 = Event(
+    event_type= "file accsess",
+    event_details= "information",
+    user = "Amina",
+    ip_address= "10.200.131.93",
+)
+
+print(event)
+print(event2)
