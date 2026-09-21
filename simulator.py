bdb4e11bfcbee23848650734136268098395b4b8
Amina <symohcpt025@student.wethinkcode.co.za>
2026-09-21 13:19:54 +0200

    feat: working on creating random versions of event details

--- simulator.py
+++ simulator.py
@@ -1,12 +1,39 @@
 from event import Event
 import random
+import ipaddress
+
+
+normal_event_types = [
+    "LOGIN_SUCCESS",
+    "LOGIN_FAILURE",
+    "FILE_ACCESS",
+    "SQL_QUERY"
+]
+def randomize_event():
+    random_event = random.choice(normal_event_types)
+    return random_event
+
+users = [
+    "AMINA MOHIUDEEN"
+]
+def randomize_user():
+    random_user = random.choice(users)
+    return random_user
+
+
+def ip_generator():
+    network = ipaddress.ip_network("10.200.131.0/24")
+    random_ip = random.choice(list(network.hosts()))
+    return random_ip
+
+
+
 def event_simulation():
     event = Event(
-    event_type= "Successful login",
+    event_type= randomize_event(),
     event_details= "information",
-    user= "Amina Mohiudeen",
-    ip_address= "10.200.131.93",)
-
+    user= randomize_user(),
+    ip_address= ip_generator(),)
     return event
 
 inital_event = event_simulation()
